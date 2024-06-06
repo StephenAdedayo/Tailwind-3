67af36be7246d4f431ff2a515d5654a0a3652069
StephenAdedayo <alustephenadedayo@gmail.com>
2024-06-06 20:28:50 +0100

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+
+
+    fontFamily:{
+      'Nunito':['Nunito', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
